7a9f9c39515aabd9673586476e15e1345326a864
ahmedmoussa <elyarmuk@hotmail.com>
2020-06-16 18:53:42 -0700

    week9 assignment

---
+++ week9_assignment/app/src/main/java/com/example/ad340_weather/api/WeeklyForecast.kt
@@ -0,0 +1,18 @@
+package com.example.ad340_weather.api
+
+import com.squareup.moshi.Json
+
+data class WeatherDescription(val main: String, val description: String, val icon: String)
+
+data class Temp(val min: Float, val max: Float)
+
+data class DailyForecast(
+    @field:Json(name = "dt") val date: Long,
+    val temp: Temp,
+    val weather: List<WeatherDescription>
+)
+
+
+//Api response for seven day forecast from OpenWeatherMap's /onecall endpoint
+
+data class WeeklyForecast(val daily: List<DailyForecast>)
---
+++ week9_assignment/app/src/main/java/com/example/ad340_weather/forecast/CurrentForecastFragment.kt
@@ -0,0 +1,85 @@
+package com.example.ad340_weather.forecast
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.*
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.ad340_weather.*
+import com.example.ad340_weather.api.CurrentWeather
+import com.example.ad340_weather.api.DailyForecast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CurrentForecastFragment : Fragment() {
+
+    private val forecastRepository = ForecastRepository()
+    private lateinit var locationRepository: LocationRepository
+    private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_current_forecast, container, false)
+        val locationName = view.findViewById<TextView>(R.id.locationName)
+        val tempText = view.findViewById<TextView>(R.id.tempText)
+        val emptyText = view.findViewById<TextView>(R.id.emptyText)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+
+        tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
+
+
+        // create the observer which update the UI response to forecast updates
+        val currentWeatherObserver = Observer<CurrentWeather> { weather ->
+            emptyText.visibility = View.GONE
+            progressBar.visibility = View.GONE
+            locationName.visibility = View.VISIBLE
+            tempText.visibility = View.VISIBLE
+
+            locationName.text = weather.name
+            tempText.text = formatTempForDisplay(
+                weather.forecast.temp,
+                tempDisplaySettingManager.getTempDisplaySetting()
+            )
+        }
+
+        forecastRepository.currentWeather.observe(viewLifecycleOwner, currentWeatherObserver)
+
+        val locationEntryButton: FloatingActionButton = view.findViewById(R.id.locationEntry)
+        locationEntryButton.setOnClickListener {
+            showLocationEntry()
+        }
+
+
+        locationRepository = LocationRepository(requireContext())
+        val savedLocationObserver = Observer<Location> { savedLocation ->
+            when (savedLocation) {
+                is Location.Zipcode -> {
+                    progressBar.visibility = View.VISIBLE
+                    forecastRepository.loadCurrentForecast(savedLocation.zipcode)
+                }
+            }
+        }
+
+        locationRepository.savedLocation.observe(viewLifecycleOwner, savedLocationObserver)
+
+
+        return view
+    }
+
+    private fun showLocationEntry() {
+        val action =
+            CurrentForecastFragmentDirections.actionCurrentForecastFragmentToLocationEntryFragment2()
+        findNavController().navigate(action)
+    }
+}
